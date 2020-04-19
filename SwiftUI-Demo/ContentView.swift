92048da5ccb115d67449e97db47f67162752718c
dilip <dilipkumar4813@gmail.com>
2020-04-19 17:15:45 +0800

    Initial commit

---
+++ SwiftUI-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Demo
+//
+//  Created by Dilip kumar on 19/04/2020.
+//  Copyright © 2020 iamdilipkumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
